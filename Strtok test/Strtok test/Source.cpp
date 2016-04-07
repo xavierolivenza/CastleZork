5ba6dcf3799ac53c144434fb1db0791af053bffa
xavierolivenza <yotolivenza@gmail.com>
2016-04-07 13:26:15 +0200

    Test for more commands

--- Strtok test/Strtok test/Source.cpp
+++ Strtok test/Strtok test/Source.cpp
@@ -2,18 +2,23 @@
 #include <string.h>
 
 void move(){
-	char fullcommand[20];
+	char fullcommand[50];
 	char firstcommand[10];
 	char secondcommand[10];
-	char command[10];
+	char thirdcommand[10];
+	char forthcommand[10];
 	char *context;//Strtok_s variable, need it to save the state of the string he analyzes. Doesn't needed with strtok.
 	printf("What do you want to do?\n");
 	gets_s(fullcommand);
 	strcpy_s(firstcommand, strtok_s(fullcommand, " ", &context));
 	strcpy_s(secondcommand, strtok_s(NULL, " ", &context));
+	strcpy_s(thirdcommand, strtok_s(NULL, " ", &context));
+	strcpy_s(forthcommand, strtok_s(NULL, " ", &context));
 	// --------------------------------------------------------------------------------------------------------------
 	printf("command1: %s\n", firstcommand);
 	printf("command2: %s\n", secondcommand);
+	printf("command3: %s\n", thirdcommand);
+	printf("command4: %s\n", forthcommand);
 }
 
 int main(){
